98aea2e0bd5cb5e7304feced6d070e110b20d18e
kojaniydracon <kojaniydracon@gmail.com>
2018-02-25 16:52:36 +0300

    sorted

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.comparator = (a,b) => a-b;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+      let selectedArr = indices.sort().map(index => this.array[index]);
+      selectedArr.sort(this.comparator);
+
+      for (let i = 0; i < indices.length; i++) {
+          this.array[indices[i]] = selectedArr[i];
+
+      }
+
+      return this.array;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
